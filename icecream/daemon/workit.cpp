1296a17458e000d71e9b4b6bffc3ab36b59aff66
Jan Holesovsky <kendy@suse.cz>
2006-01-26 13:56:30 +0000

    Improve out of memory detection by checking stderr against "virtual memory exhausted: Cannot allocate memory" message.
    svn path=/trunk/kdenonbeta/icecream/; revision=502547

--- icecream/daemon/workit.cpp
+++ icecream/daemon/workit.cpp
@@ -308,7 +308,9 @@ int work_it( CompileJob &j,
 
                     if ( status ) {
                         unsigned long int mem_used = ( ru.ru_minflt + ru.ru_majflt ) * getpagesize() / 1024;
-                        if ( mem_used * 100 > 85 * mem_limit * 1024 ) {
+                        if ( mem_used * 100 > 85 * mem_limit * 1024 ||
+                             str_err.find( "virtual memory exhausted: Cannot allocate memory" ) != string::npos )
+                        {
                             // the relation between ulimit and memory used is pretty thin ;(
                             return EXIT_OUT_OF_MEMORY;
                         }
