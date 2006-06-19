d6ed5bb4b1757c383209eb6f09755247e44c71ac
Stephan Kulow <coolo@kde.org>
2006-06-19 13:14:13 +0000

    the -x c is necessary
    svn path=/trunk/icecream/; revision=552924

--- daemon/workit.cpp
+++ daemon/workit.cpp
@@ -207,7 +207,7 @@ int work_it( CompileJob &j,
 
         int argc = list.size();
         argc++; // the program
-        argc += 3; // - -o file.o
+        argc += 5; // -x c - -o file.o
         argc += 4; // gpc parameters
         char **argv = new char*[argc + 1];
 	int i = 0;
@@ -224,6 +224,8 @@ int work_it( CompileJob &j,
               it != list.end(); ++it) {
             argv[i++] = strdup( it->c_str() );
         }
+        argv[i++] = strdup("-x");
+        argv[i++] = strdup((j.language() == CompileJob::Lang_CXX) ? "c++" : "c");
         argv[i++] = strdup( "-" );
         argv[i++] = strdup( "-o" );
         argv[i++] = tmp_output;
