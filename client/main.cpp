d6164176d6a081dfa933136bca3201e83cf91160
Stephan Kulow <coolo@kde.org>
2006-09-04 08:41:51 +0000

    if ICECC=no is set, then don't try to talk to the local daemon at all avoiding all kind of problems. If your icecream is hosed, you want a quick way out :)
    svn path=/trunk/icecream/; revision=580682

--- client/main.cpp
+++ client/main.cpp
@@ -215,16 +215,17 @@ int main(int argc, char **argv)
 
     CompileJob job;
 
+    local |= analyse_argv( argv, job );
+
     /* if ICECC is set to no, then run job locally */
     if ( getenv( "ICECC" ) ) {
 	string icecc;
 	icecc = getenv( "ICECC" );
-        if ( icecc == "no" ) {
-	    local = true;
-        }
+        /* if the environment variable is set, the user wants to debug if a compile
+           failure is to blame to icecream, so make sure we do the least we can. */
+        if ( icecc == "no" )
+            return build_local( job, 0 );
     }
-
-    local |= analyse_argv( argv, job );
 
     MsgChannel *local_daemon = Service::createChannel( "127.0.0.1", 10245, 0/*timeout*/);
     if ( ! local_daemon ) {
