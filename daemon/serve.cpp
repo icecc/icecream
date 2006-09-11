34a82259d9f1c18bd85fd42e062f5a562f1b71d5
Dirk Mueller <mueller@kde.org>
2006-09-11 19:18:31 +0000

    make sure we can handle a closed pipe here
    svn path=/branches/work/icecream-make-it-cool/; revision=583221

--- daemon/serve.cpp
+++ daemon/serve.cpp
@@ -197,7 +197,7 @@ int handle_connection( const string &basedir, CompileJob *job,
             job_stat[JobStatistics::out_uncompressed] = st.st_size;
 
         /* wake up parent and tell him that compile finished */
-        write( out_fd, job_stat, sizeof( job_stat ) );
+        send( out_fd, job_stat, sizeof( job_stat ), MSG_NOSIGNAL );
         close( out_fd );
 
         if ( rmsg.status == 0 ) {
