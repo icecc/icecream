5672de797472345d331fd59327bc4a7d5f419c6d
Frank Osterfeld <frank.osterfeld@kdemail.net>
2009-03-25 22:39:04 +0000

    assert current_kids is > 0 before decrementing (it's unsigned)
    svn path=/trunk/icecream/; revision=944682

--- daemon/main.cpp
+++ daemon/main.cpp
@@ -777,6 +777,7 @@ bool Daemon::handle_transfer_env_done( Client *client )
     string current = client->outfile;
     client->outfile.clear();
     client->child_pid = -1;
+    assert( current_kids > 0 );
     current_kids--;
 
     log_error() << "installed_size: " << installed_size << endl;
