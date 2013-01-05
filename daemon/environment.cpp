4e5f12ab8370011c475ddd368e4c07d17c9ba857
Luboš Luňák <l.lunak@suse.cz>
2013-01-05 14:49:55 +0100

    do not remove env. basedir itself
    Since the change to run as non-root, the daemon cannot remove e.g.
    /var/cache/icecream because of no rights to modify /var/cache.

--- daemon/environment.cpp
+++ daemon/environment.cpp
@@ -137,17 +137,32 @@ static bool exec_and_wait( const char *const argv[] )
     _exit(execv(argv[0], const_cast<char *const *>(argv)));
 }
 
+// Removes everything in the directory recursively, but not the directory itself.
+static bool cleanup_directory( const string& directory )
+{
+    DIR* dir = opendir( directory.c_str());
+    if( dir == NULL )
+        return false;
+    while( dirent* f = readdir( dir )) {
+        if( strcmp( f->d_name, "." ) == 0 || strcmp( f->d_name, ".." ) == 0 )
+            continue;
+        string fullpath = directory + '/' + f->d_name;
+        if( unlink( fullpath.c_str()) != 0 ) {
+            if( !cleanup_directory( fullpath ) || rmdir( fullpath.c_str()) != 0 ) {
+                return false;
+            }
+        }
+    }
+    closedir( dir );
+    return true;
+}
+
 bool cleanup_cache( const string &basedir )
 {
     flush_debug();
 
-    // make sure it ends with '/' to not fall into symlink traps
-    string bdir = basedir + '/';
-    const char *const argv[] = {
-        "/bin/rm", "-rf", "--", bdir.c_str(), NULL
-    };
-
-    bool ret = exec_and_wait( argv );
+    if ( access( basedir.c_str(), R_OK ) == 0 && !cleanup_directory( basedir ))
+        return false;
 
     if ( mkdir( basedir.c_str(), 0755 ) && errno != EEXIST ) {
         if ( errno == EPERM )
@@ -157,7 +172,7 @@ bool cleanup_cache( const string &basedir )
         return false;
     }
 
-    return ret;
+    return true;
 }
 
 Environments available_environmnents(const string &basedir)
