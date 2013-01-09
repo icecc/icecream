8a3c82bb4dd9bf5bc9ff4eb1dc80f4568c92afec
Stephan Kulow <coolo@suse.de>
2013-01-09 14:26:26 +0100

    mild relicensing, the code taken from ksysguardd is really a minor part by now

--- daemon/load.cpp
+++ daemon/load.cpp
@@ -2,9 +2,10 @@
     Copyright (c) 1999, 2000 Chris Schlaeger <cs@kde.org>
     Copyright (c) 2003 Stephan Kulow <coolo@kde.org>
 
-    This program is free software; you can redistribute it and/or
-    modify it under the terms of version 2 of the GNU General Public
-    License as published by the Free Software Foundation.
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
