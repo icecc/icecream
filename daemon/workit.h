08a9a6ec4ade1e011353147120994015f44a1201
Luboš Luňák <l.lunak@suse.cz>
2013-02-20 23:06:33 +0100

    update the text of the GPL (not the version)
    This merely updates the license text to the latest GPLv2 text (fixing
    the FSF address). This does not do any actual license change.

--- daemon/workit.h
+++ daemon/workit.h
@@ -3,6 +3,7 @@
 
     Copyright (c) 2004 Stephan Kulow <coolo@suse.de>
                   2002, 2003 by Martin Pool <mbp@samba.org>
+
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation; either version 2 of the License, or
@@ -10,12 +11,12 @@
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
 #ifndef _WORKIT_H
