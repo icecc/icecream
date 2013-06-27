icecc/icecream

commit 203b51b588fa2e550feeeef73d91e6d72afeafa9

Add support for libcap-ng
Signed-off-by: Rodrigo Belem <rodrigo.belem@gmail.com>

daemon/main.cpp
@@ -78,6 +78,10 @@
 #  define RUSAGE_CHILDREN (-1)
 #endif
 
+#ifdef HAVE_LIBCAP_NG
+#  include <cap-ng.h>
+#endif
+
 #include <deque>
 #include <map>
 #include <algorithm>
@@ -422,9 +426,17 @@ struct Daemon
     unsigned int current_kids;
 
     Daemon() {
+        struct passwd *pw = getpwnam("icecc");
+        if (pw) {
+            user_uid = pw->pw_uid;
+            user_gid = pw->pw_gid;
+        } else {
+            log_perror ("Error: no icecc user on system. Falling back to nobody.");
+            user_uid = 65534;
+            user_gid = 65533;
+        }
+
         envbasedir = "/tmp/icecc-envs";
-        nobody_uid = 65534;
-        nobody_gid = 65533;
         tcp_listen_fd = -1;
 	unix_listen_fd = -1;
         new_client_id = 0;
@@ -524,7 +536,7 @@ bool Daemon::setup_listen_fds()
     memset(&myaddr, 0, sizeof(myaddr));
     myaddr.sun_family = AF_UNIX;
     mode_t old_umask = -1U;
-    if (getuid()==0) {
+    if (access("/var/run/icecc", R_OK|W_OK|X_OK) == 0) {
         strncpy(myaddr.sun_path, "/var/run/icecc/iceccd.socket", sizeof(myaddr.sun_path)-1);
         unlink(myaddr.sun_path);
         old_umask = umask(0);
@@ -1778,6 +1790,12 @@ int main( int argc, char ** argv )
                 chown("/var/run/icecc", d.user_uid, d.user_gid);
             }
         }
+#ifdef HAVE_LIBCAP_NG
+        capng_clear(CAPNG_SELECT_BOTH);
+        capng_update(CAPNG_ADD, (capng_type_t)(CAPNG_EFFECTIVE|CAPNG_PERMITTED), CAP_SYS_CHROOT);
+        capng_change_id(d.user_uid, d.user_gid, CAPNG_NO_FLAG);
+        capng_apply(CAPNG_SELECT_BOTH);
+#endif
     } else {
         d.noremote = true;
     }

configure.in
@@ -164,6 +164,14 @@ fi
 AC_CHECK_LIB(resolv, hstrerror, , , [-lnsl -lsocket])
 AC_CHECK_LIB(resolv, inet_aton, , , [-lnsl -lsocket])
 
+AC_CHECK_LIB(cap-ng, capng_clear, have_libcap_ng=yes, have_libcap_ng=no)
+AM_CONDITIONAL(HAVE_LIBCAP_NG, test x$have_libcap_ng = xyes)
+
+if test x$have_libcap_ng = xyes ; then
+    LIBS="-lcap-ng $LIBS"
+    AC_DEFINE(HAVE_LIBCAP_NG,1,[Define if system has libcap-ng installed])
+fi
+
 AC_CHECK_FUNCS([setsid flock lockf hstrerror strerror setuid setreuid])
 AC_CHECK_FUNCS([getuid geteuid mcheck wait4 wait3 waitpid setgroups getcwd])
 AC_CHECK_FUNCS([snprintf vsnprintf vasprintf asprintf getcwd getwd])

daemon/Makefile.am
@@ -3,6 +3,6 @@ INCLUDES = -I$(srcdir)/../services
 
 sbin_PROGRAMS = iceccd
 iceccd_SOURCES = ncpus.c main.cpp serve.cpp workit.cpp environment.cpp load.cpp
-iceccd_LDADD = ../services/libicecc.la $(LIB_KINFO)
+iceccd_LDADD = ../services/libicecc.la $(LIB_KINFO) $(CAPNG_LDADD)
 noinst_HEADERS = environment.h load.h ncpus.h serve.h workit.h
 

daemon/environment.cpp
@@ -284,6 +284,7 @@ size_t setup_env_cache(const string &basedir, string &native_environment, uid_t 
     }
     // else
 
+#ifndef HAVE_LIBCAP_NG
     if ( setgid( user_gid ) < 0) {
       log_perror("setgid failed");
       _exit(143);
@@ -292,6 +293,7 @@ size_t setup_env_cache(const string &basedir, string &native_environment, uid_t 
       log_perror("setuid failed");
       _exit (142);
     }
+#endif
 
     if ( chdir( nativedir.c_str() ) ) {
          log_perror( "chdir" );
@@ -393,6 +395,7 @@ pid_t start_install_environment( const std::string &basename, const std::string 
         return pid;
     }
     // else
+#ifndef HAVE_LIBCAP_NG
     if ( setgid( user_gid ) < 0) {
       log_perror("setgid fails");
       _exit(143);
@@ -401,6 +404,7 @@ pid_t start_install_environment( const std::string &basename, const std::string 
       log_perror("setuid fails");
       _exit (142);
     }
+#endif
 
     // reset SIGPIPE and SIGCHILD handler so that tar
     // isn't confused when gzip/bzip2 aborts
@@ -501,6 +505,18 @@ error_client( MsgChannel *client, string error )
 
 void chdir_to_environment( MsgChannel *client, const string &dirname, uid_t user_uid, gid_t user_gid )
 {
+#ifdef HAVE_LIBCAP_NG
+    if ( chdir( dirname.c_str() ) < 0 ) {
+        error_client( client, string( "chdir to " ) + dirname + "failed" );
+        log_perror("chdir() failed" );
+        _exit(145);
+    }
+    if ( chroot( dirname.c_str() ) < 0 ) {
+        error_client( client, string( "chroot " ) + dirname + "failed" );
+        log_perror("chroot() failed" );
+        _exit(144);
+    }
+#else
     if ( getuid() == 0 ) {
         // without the chdir, the chroot will escape the
         // jail right away
@@ -532,6 +548,7 @@ void chdir_to_environment( MsgChannel *client, const string &dirname, uid_t user
             trace() << "chdir to " << dirname << endl;
         }
     }
+#endif
 }
 
 // Verify that the environment works by simply running the bundled bin/true.

daemon/workit.cpp
@@ -151,11 +151,13 @@ int work_it( CompileJob &j, unsigned int job_stat[], MsgChannel* client,
     } else if ( pid == 0 ) {
 
         setenv( "PATH", "usr/bin", 1 );
+#ifndef HAVE_LIBCAP_NG
         // Safety check
         if (getuid() == 0 || getgid() == 0) {
             error_client( client, "UID is 0 - aborting." );
             _exit(142);
         }
+#endif
 
 
 #ifdef RLIMIT_AS

commit 714e64160f2002693d760b1144f7ece8b74fd203

fix /var/run/icecc handling
Was severely broken by 203b51b588fa2e550feeeef73d91e6d72afeafa9.
It first created /var/run/icecc, which was successful and didn't have
the follow-up chown/chmod, so they ended up as root-owned, because
this happened before dropping privileges using libcap-ng. This caused
the later access() check for it to fail, and ended up using the user
path, which however crashed because of there being no $HOME set.
So after every boot, if /var/run was empty, the first launch failed.
All following launches were successful, because the mkdir() failed,
leaving to proper chown/chmod.

Fix by always doing chown/chmod unconditionally. Also do not use
the access() check for detecting if it's the system or user way of launching.

daemon/main.cpp
@@ -543,11 +543,16 @@ bool Daemon::setup_listen_fds()
     memset(&myaddr, 0, sizeof(myaddr));
     myaddr.sun_family = AF_UNIX;
     mode_t old_umask = -1U;
-    if (access("/var/run/icecc", R_OK|W_OK|X_OK) == 0) {
+#ifdef HAVE_LIBCAP_NG
+    // We run as system daemon.
+    if (capng_have_capability( CAPNG_PERMITTED, CAP_SYS_CHROOT )) {
+#else
+    if (getuid()==0) {
+#endif
         strncpy(myaddr.sun_path, "/var/run/icecc/iceccd.socket", sizeof(myaddr.sun_path)-1);
         unlink(myaddr.sun_path);
         old_umask = umask(0);
-    } else {
+    } else { // Started by user.
         strncpy(myaddr.sun_path, getenv("HOME"), sizeof(myaddr.sun_path)-1);
         strncat(myaddr.sun_path, "/.iceccd.socket", sizeof(myaddr.sun_path)-1-strlen(myaddr.sun_path));
         unlink(myaddr.sun_path);
@@ -1785,22 +1790,16 @@ int main( int argc, char ** argv )
 
     if (getuid() == 0) {
         if (!logfile.length() && detach) {
-            if (mkdir("/var/log/icecc", S_IRWXU|S_IRGRP|S_IXGRP|S_IROTH|S_IXOTH)) {
-                if (errno == EEXIST) {
-                    chmod("/var/log/icecc", S_IRWXU|S_IRGRP|S_IXGRP|S_IROTH|S_IXOTH);
-                    chown("/var/log/icecc", d.user_uid, d.user_gid);
-                }
-            }
-
+            mkdir("/var/log/icecc", S_IRWXU|S_IRGRP|S_IXGRP|S_IROTH|S_IXOTH);
+            chmod("/var/log/icecc", S_IRWXU|S_IRGRP|S_IXGRP|S_IROTH|S_IXOTH);
+            chown("/var/log/icecc", d.user_uid, d.user_gid);
             logfile = "/var/log/icecc/iceccd.log";
         }
 
-        if (mkdir("/var/run/icecc", S_IRWXU|S_IRGRP|S_IXGRP|S_IROTH|S_IXOTH) == -1) {
-            if (errno == EEXIST) {
-                chmod("/var/run/icecc", S_IRWXU|S_IRGRP|S_IXGRP|S_IROTH|S_IXOTH);
-                chown("/var/run/icecc", d.user_uid, d.user_gid);
-            }
-        }
+        mkdir("/var/run/icecc", S_IRWXU|S_IRGRP|S_IXGRP|S_IROTH|S_IXOTH);
+        chmod("/var/run/icecc", S_IRWXU|S_IRGRP|S_IXGRP|S_IROTH|S_IXOTH);
+        chown("/var/run/icecc", d.user_uid, d.user_gid);
+
 #ifdef HAVE_LIBCAP_NG
         capng_clear(CAPNG_SELECT_BOTH);
         capng_update(CAPNG_ADD, (capng_type_t)(CAPNG_EFFECTIVE|CAPNG_PERMITTED), CAP_SYS_CHROOT);