icecc/icecream

commit 76effd20a3b62644cbbbcf50e906b914e4297e6f

change code to support color diagnostics also for GCC
Since it's new in GCC 4.8, for now require ICECC_GCC_COLORS set
to actually make it work like it would automatically with Clang
(icecc doesn't know what compiler version it's run with), the default
can be flipped somewhen later.

client/util.cpp
@@ -228,25 +228,40 @@ bool dcc_lock_host(int &lock_fd)
     return false;
 }
 
-bool colorify_possible()
+bool color_output_possible()
 {
     const char* term_env = getenv("TERM");
 
     return isatty(2) && term_env && strcasecmp(term_env, "DUMB");
 }
 
+bool compiler_supports_colors(const CompileJob &job)
+{
+    // Clang has coloring.
+    if (compiler_is_clang(job)) {
+        return true;
+    }
+    // GCC has it since 4.8, but that'd require detecting what GCC
+    // version is used for the actual compile, so use env.var. for now
+    // and somewhen later switch the default.
+    if (const char *icecc_gcc_colors = getenv("ICECC_GCC_COLORS")) {
+        return (*icecc_gcc_colors != '\0') && (*icecc_gcc_colors != '0');
+    }
+    return false;
+}
+
+// Whether icecream should add colors to the compiler output.
 bool colorify_wanted(const CompileJob &job)
 {
-    // Clang has coloring, and an explicit option to force it even if output is not a tty.
-    if (compiler_is_clang(job)) {
-        return false;
+    if (compiler_supports_colors(job)) {
+        return false; // -fcolor-diagnostics handling lets the compiler do it itself
     }
 
     if (getenv("EMACS")) {
         return false;
     }
 
-    return colorify_possible();
+    return color_output_possible();
 }
 
 void colorify_output(const string &_s_ccout)

client/arg.cpp
@@ -562,9 +562,13 @@ bool analyse_argv(const char * const *argv, CompileJob &job, bool icerun, list<s
         always_local = true;
     }
 
-    // redirecting Clang's output will turn off its automatic coloring, so force it, unless disabled
-    if (compiler_is_clang(job) && colorify_possible() && !fno_color_diagnostics) {
-        args.append("-fcolor-diagnostics", Arg_Rest);
+    // redirecting compiler's output will turn off its automatic coloring, so force it
+    // when it would be used, unless explicitly disabled
+    if (compiler_supports_colors(job) && !fno_color_diagnostics) {
+        if (compiler_is_clang(job))
+            args.append("-fcolor-diagnostics", Arg_Rest);
+        else
+            args.append("-fdiagnostics-color", Arg_Rest); // GCC
     }
 
     job.setFlags(args);

client/main.cpp
@@ -92,6 +92,7 @@ static void dcc_show_usage(void)
         "   ICECC_CXX                  set C++ compiler name (default g++).\n"
         "   ICECC_CLANG_REMOTE_CPP     set to 1 or 0 to override remote precompiling with clang\n"
         "                              (requires clang -frewrite-includes option).\n"
+        "   ICECC_GCC_COLORS           set to 1 to automatically use colors with GCC 4.8+ when posible.\n"
         "   ICECC_IGNORE_UNVERIFIED    if set, hosts where environment cannot be verified are not used.\n"
         "   ICECC_EXTRAFILES           additional files used in the compilation.\n"
         "\n");

client/util.h
@@ -32,7 +32,7 @@ extern std::string find_basename(const std::string &sfile);
 extern std::string find_prefix(const std::string &basename);
 extern void colorify_output(const std::string &s_ccout);
 extern bool colorify_wanted(const CompileJob &job);
-extern bool colorify_possible();
+extern bool compiler_supports_colors(const CompileJob &job);
 extern bool ignore_unverified();
 extern int resolve_link(const std::string &file, std::string &resolved);
 

commit c38fbd35ec42f011c24d2610325b492e24deb4be

enable colors only if supported by output
Regression from 76effd20a3b62644cbbbcf50e906b914e4297e6f.

client/util.cpp
@@ -239,6 +239,9 @@ bool color_output_possible()
 
 bool compiler_has_color_output(const CompileJob &job)
 {
+    if (!color_output_possible())
+        return false;
+
     // Clang has coloring.
     if (compiler_is_clang(job)) {
         return true;