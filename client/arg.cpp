5a88cb2ccdb239d0c4d929241e5f0bce74088528
Jürgen Hunold <jhunold@gmx.eu>
2014-01-20 12:56:16 +0100

    just set language for "-x c++" and "-x c"

--- client/arg.cpp
+++ client/arg.cpp
@@ -257,7 +257,6 @@ bool analyse_argv(const char * const *argv, CompileJob &job, bool icerun, list<s
                 always_local = true;
                 args.append(a, Arg_Local);
             } else if (!strcmp(a, "-x")) {
-                bool local = false;
                 const char *opt = argv[++i];
 
                 if (!strcmp, opt, "c++") {
@@ -265,18 +264,11 @@ bool analyse_argv(const char * const *argv, CompileJob &job, bool icerun, list<s
                 } else if (!strcmp, opt, "c") {
                     job.setLanguage(CompileJob::Lang_C);
                 } else {
-                    local = true;
 #if CLIENT_DEBUG
                     log_info() << "unsupported -x option; running locally" << endl;
 #endif
-                }
-
-                if (local) {
                     always_local = true;
                     args.append(a, Arg_Local);
-                } else {
-                    args.append(a, Arg_Remote);
-                    args.append(opt, Arg_Remote);
                 }
             } else if (!strcmp(a, "-march=native") || !strcmp(a, "-mcpu=native")
                        || !strcmp(a, "-mtune=native")) {
