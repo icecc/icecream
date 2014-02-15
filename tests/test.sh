e8b47a47040f0ea588aaa4b412b85820c82b2ad1
Luboš Luňák <l.lunak@centrum.cz>
2014-02-16 00:20:57 +0100

    make test check logs for various errors and expected messages

--- tests/test.sh
+++ tests/test.sh
@@ -12,6 +12,11 @@ mkdir -p "$testdir"
 
 start_ice()
 {
+    echo -n >"$testdir"/scheduler.log
+    echo -n >"$testdir"/localice.log
+    echo -n >"$testdir"/remoteice1.log
+    echo -n >"$testdir"/remoteice2.log
+    echo -n >"$testdir"/icecc.log
     "$prefix"/sbin/icecc-scheduler -p 8767 -l "$testdir"/scheduler.log -v -v -v &
     scheduler_pid=$!
     echo $scheduler_pid > "$testdir"/scheduler.pid
@@ -24,20 +29,41 @@ start_ice()
     ICECC_TEST_SOCKET="$testdir"/socket-remoteice2 "$prefix"/sbin/iceccd -p 10247 -s localhost:8767 -b "$testdir"/envs-remoteice2 -l "$testdir"/remoteice2.log -N remoteice2 -v -v -v &
     remoteice2_pid=$!
     echo $remoteice2_pid > "$testdir"/remoteice2.pid
-    sleep 5
-    if ! kill -0 $scheduler_pid; then
-        echo Scheduler start failure.
-        stop_ice
-        exit 1
-    fi
-    for daemon in localice remoteice1 remoteice2; do
-        pid=${daemon}_pid
-        if ! kill -0 ${!pid}; then
-            echo Daemon $daemon start failure.
-            stop_ice
+    notready=
+    sleep 1
+    for time in `seq 1 5`; do
+        notready=
+        if ! kill -0 $scheduler_pid; then
+            echo Scheduler start failure.
+            stop_ice 0
             exit 1
         fi
+        for daemon in localice remoteice1 remoteice2; do
+            pid=${daemon}_pid
+            if ! kill -0 ${!pid}; then
+                echo Daemon $daemon start failure.
+                stop_ice 0
+                exit 1
+            fi
+            if ! grep -q "Connected to scheduler" "$testdir"/${daemon}.log; then
+                # ensure log file flush
+                kill -HUP ${!pid}
+                grep -q "Connected to scheduler" "$testdir"/${daemon}.log || notready=1
+            fi
+        done
+        if test -z "$notready"; then
+            break;
+        fi
+        sleep 1
     done
+    if test -n "$notready"; then
+        echo Icecream not ready, aborting.
+        stop_ice 0
+        exit 1
+    fi
+    flush_logs
+    check_log_error remoteice1 "Cannot use chroot, no remote jobs accepted."
+    check_log_error remoteice2 "Cannot use chroot, no remote jobs accepted."
 }
 
 stop_ice()
@@ -76,23 +102,53 @@ stop_ice()
 }
 
 # First argument is the expected output file, if any (otherwise specify "").
+# Second argument is "remote" (should be compiled on a remote host) or "local" (cannot be compiled remotely).
 # Rest is the command to pass to icecc.
 # Command will be run both locally and using icecc and results compared.
 run_ice()
 {
     output="$1"
     shift
+    remote_type="$1"
+    shift
+
+    reset_logs local "$@"
     echo Running: "$@"
     ICECC_TEST_SOCKET="$testdir"/socket-localice ICECC_TEST_REMOTEBUILD=1 ICECC_PREFERRED_HOST=localice ICECC_DEBUG=debug ICECC_LOGFILE="$testdir"/icecc.log "$prefix"/bin/icecc "$@"
     localice_exit=$?
     if test -n "$output"; then
         mv "$output" "$output".localice
     fi
+    flush_logs
+    check_logs_for_generic_errors
+    if test "$remote_type" = "remote"; then
+        check_log_message icecc "building myself, but telling localhost"
+        check_log_error icecc "<building_local>"
+    else
+        check_log_message icecc "<building_local>"
+        check_log_error icecc "building myself, but telling localhost"
+    fi
+    check_log_error icecc "Have to use host 127.0.0.1:10246"
+    check_log_error icecc "Have to use host 127.0.0.1:10247"
+
+    reset_logs remote "$@"
     ICECC_TEST_SOCKET="$testdir"/socket-localice ICECC_TEST_REMOTEBUILD=1 ICECC_PREFERRED_HOST=remoteice1 ICECC_DEBUG=debug ICECC_LOGFILE="$testdir"/icecc.log "$prefix"/bin/icecc "$@"
     remoteice_exit=$?
     if test -n "$output"; then
         mv "$output" "$output".remoteice
     fi
+    flush_logs
+    check_logs_for_generic_errors
+    if test "$remote_type" = "remote"; then
+        check_log_message icecc "Have to use host 127.0.0.1:10246"
+        check_log_error icecc "<building_local>"
+    else
+        check_log_message icecc "<building_local>"
+        check_log_error icecc "Have to use host 127.0.0.1:10246"
+    fi
+    check_log_error icecc "Have to use host 127.0.0.1:10247"
+    check_log_error icecc "building myself, but telling localhost"
+
     "$@"
     normal_exit=$?
     if test $localice_exit -ne $normal_exit; then
@@ -122,22 +178,87 @@ run_ice()
     fi
 }
 
-rm -f "$testdir"/scheduler.log
-rm -f "$testdir"/localice.log
-rm -f "$testdir"/remoteice1.log
-rm -f "$testdir"/remoteice2.log
-rm -f "$testdir"/icecc.log
+reset_logs()
+{
+    type="$1"
+    shift
+    # in case icecc.log doesn't exit, avoid error message
+    touch "$testdir"/icecc.log
+    for log in scheduler localice remoteice1 remoteice2 icecc; do
+        # save (append) previous log
+        cat "$testdir"/${log}.log >> "$testdir"/${log}_all.log
+        # and start a new one
+        echo ============== > "$testdir"/${log}.log
+        echo "Test ($type): $@" >> "$testdir"/${log}.log
+        echo ============== >> "$testdir"/${log}.log
+        if test "$log" != icecc; then
+            pid=${log}_pid
+            kill -HUP ${!pid}
+        fi
+    done
+}
 
+finish_logs()
+{
+    for log in scheduler localice remoteice1 remoteice2 icecc; do
+        cat "$testdir"/${log}.log >> "$testdir"/${log}_all.log
+        rm -f "$testdir"/${log}.log
+    done
+}
+
+flush_logs()
+{
+    for daemon in scheduler localice remoteice1 remoteice2; do
+        pid=${daemon}_pid
+        kill -HUP ${!pid}
+    done
+}
+
+check_logs_for_generic_errors()
+{
+    check_log_error scheduler "that job isn't handled by"
+    check_log_error scheduler "the server isn't the same for job"
+}
+
+check_log_error()
+{
+    log="$1"
+    if grep -q "$2" "$testdir"/${log}.log; then
+        echo "Error, $log log contains error: $2"
+        exit 1
+    fi
+}
+
+check_log_message()
+{
+    log="$1"
+    if ! grep -q "$2" "$testdir"/${log}.log; then
+        echo "Error, $log log does not contain: $2"
+        exit 1
+    fi
+}
+
+rm -f "$testdir"/scheduler_all.log
+rm -f "$testdir"/localice_all.log
+rm -f "$testdir"/remoteice1_all.log
+rm -f "$testdir"/remoteice2_all.log
+rm -f "$testdir"/icecc_all.log
+
+echo Starting icecream.
 stop_ice 0
 start_ice
+check_logs_for_generic_errors
 
 echo Starting tests.
 echo ===============
 
-run_ice "$testdir/plain.o" g++ -Wall -Werror -c plain.cpp -o "$testdir/"plain.o
-run_ice "$testdir/plain.ii" g++ -Wall -Werror -E plain.cpp -o "$testdir/"plain.ii
-run_ice "" g++ -c nonexistent.cpp
+run_ice "$testdir/plain.o" "remote" g++ -Wall -Werror -c plain.cpp -o "$testdir/"plain.o
+run_ice "$testdir/plain.ii" "local" g++ -Wall -Werror -E plain.cpp -o "$testdir/"plain.ii
+run_ice "" "remote" g++ -c nonexistent.cpp
 
 stop_ice 1
+check_logs_for_generic_errors
+finish_logs
+
 echo All tests OK.
 echo =============
