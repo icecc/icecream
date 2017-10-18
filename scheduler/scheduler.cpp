ca30b0f25cb98c8effd80c22c4db6487189fc524
Ken Faiczak <kfaiczak@sandvine.com>
2017-10-18 09:53:54 -0400

    fixup more bind issues

--- scheduler/scheduler.cpp
+++ scheduler/scheduler.cpp
@@ -1701,7 +1701,7 @@ static int open_broad_listener(int port)
     myaddr.sin_port = htons(port);
     myaddr.sin_addr.s_addr = INADDR_ANY;
 
-    if (bind(listen_fd, (struct sockaddr *) &myaddr, sizeof(myaddr)) < 0) {
+    if (::bind(listen_fd, (struct sockaddr *) &myaddr, sizeof(myaddr)) < 0) {
         log_perror("bind()");
         return -1;
     }
@@ -1738,7 +1738,7 @@ static int open_tcp_listener(short port)
     myaddr.sin_port = htons(port);
     myaddr.sin_addr.s_addr = INADDR_ANY;
 
-    if (bind(fd, (struct sockaddr *) &myaddr, sizeof(myaddr)) < 0) {
+    if (::bind(fd, (struct sockaddr *) &myaddr, sizeof(myaddr)) < 0) {
         log_perror("bind()");
         return -1;
     }
