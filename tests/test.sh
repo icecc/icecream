24fbfa18cee01269cf560d6f90f7c9bdf7c07c8b
Luboš Luňák <l.lunak@centrum.cz>
2017-12-13 09:16:53 +0100

    do not explicitly check (and ignore) address sanitizer failures in a test
    It's been already fixed, and moreover such a failure shouldn't be ignored
    anyway.

--- tests/test.sh
+++ tests/test.sh
@@ -1070,14 +1070,6 @@ zero_local_jobs_test()
     ICECC_TEST_SOCKET="$testdir"/socket-localice ICECC_TEST_REMOTEBUILD=1 ICECC_PREFERRED_HOST=remoteice1 ICECC_DEBUG=debug ICECC_LOGFILE="$testdir"/icecc.log $valgrind "${icecc}" $TESTCXX -Wall -Werror -c testfunc.cpp -o "${testdir}/testfunc.o"
     if [[ $? -ne 0 ]]; then
         echo "failed to build testfunc.o"
-        grep -q "AddressSanitizer failed to allocate"  "$testdir"/iceccdstderr_remoteice1.log
-        if [[ $? ]]; then
-            echo "address sanitizer broke, skipping test"
-            skipped_tests="$skipped_tests zero_local_jobs_test"
-            mark_logs local "skipping zero_local_jobs_test"
-            start_iceccd localice --no-remote -m 2
-            return 0
-        fi
         stop_ice 0
         abort_tests
     fi
