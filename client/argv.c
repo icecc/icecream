17c60399555bf38435638ed01d452fbb2302a5e4
Daniel van Gerpen <daniel@vangerpen.de>
2017-12-28 13:49:53 +0100

    buildargv: reindent with clang-format (no functional change)

--- client/argv.c
+++ client/argv.c
@@ -188,99 +188,99 @@ static char **buildargv (const char *input)
   char **argv = NULL;
   char **nargv;
 
-      copybuf = (char *) malloc (strlen (input) + 1);
-      /* Is a do{}while to always execute the loop once.  Always return an
-	 argv, even for null strings.  See NOTES above, test case below. */
-      do
-	{
-	  /* Pick off argv[argc] */
-	  consume_whitespace (&input);
+  copybuf = (char *) malloc (strlen (input) + 1);
+  /* Is a do{}while to always execute the loop once.  Always return an
+     argv, even for null strings.  See NOTES above, test case below. */
+  do
+    {
+      /* Pick off argv[argc] */
+      consume_whitespace (&input);
 
-	  if ((maxargc == 0) || (argc >= (maxargc - 1)))
-	    {
-	      /* argv needs initialization, or expansion */
-	      if (argv == NULL)
-		{
-		  maxargc = INITIAL_MAXARGC;
-		  nargv = (char **) malloc (maxargc * sizeof (char *));
-		}
-	      else
-		{
-		  maxargc *= 2;
-		  nargv = (char **) realloc (argv, maxargc * sizeof (char *));
-		}
-	      argv = nargv;
-	      argv[argc] = NULL;
-	    }
-	  /* Begin scanning arg */
-	  arg = copybuf;
-	  while (*input != EOS)
-	    {
-	      if (isspace (*input) && !squote && !dquote && !bsquote)
-		{
-		  break;
-		}
-	      else
-		{
-		  if (bsquote)
-		    {
-		      bsquote = 0;
-		      *arg++ = *input;
-		    }
-		  else if (*input == '\\')
-		    {
-		      bsquote = 1;
-		    }
-		  else if (squote)
-		    {
-		      if (*input == '\'')
-			{
-			  squote = 0;
-			}
-		      else
-			{
-			  *arg++ = *input;
-			}
-		    }
-		  else if (dquote)
-		    {
-		      if (*input == '"')
-			{
-			  dquote = 0;
-			}
-		      else
-			{
-			  *arg++ = *input;
-			}
-		    }
-		  else
-		    {
-		      if (*input == '\'')
-			{
-			  squote = 1;
-			}
-		      else if (*input == '"')
-			{
-			  dquote = 1;
-			}
-		      else
-			{
-			  *arg++ = *input;
-			}
-		    }
-		  input++;
-		}
-	    }
-	  *arg = EOS;
-	  argv[argc] = strdup (copybuf);
-	  argc++;
-	  argv[argc] = NULL;
+      if ((maxargc == 0) || (argc >= (maxargc - 1)))
+        {
+          /* argv needs initialization, or expansion */
+          if (argv == NULL)
+            {
+              maxargc = INITIAL_MAXARGC;
+              nargv = (char **) malloc (maxargc * sizeof (char *));
+            }
+          else
+            {
+              maxargc *= 2;
+              nargv = (char **) realloc (argv, maxargc * sizeof (char *));
+            }
+          argv = nargv;
+          argv[argc] = NULL;
+        }
+      /* Begin scanning arg */
+      arg = copybuf;
+      while (*input != EOS)
+        {
+          if (isspace (*input) && !squote && !dquote && !bsquote)
+            {
+              break;
+            }
+          else
+            {
+              if (bsquote)
+                {
+                  bsquote = 0;
+                  *arg++ = *input;
+                }
+              else if (*input == '\\')
+                {
+                  bsquote = 1;
+                }
+              else if (squote)
+                {
+                  if (*input == '\'')
+                    {
+                      squote = 0;
+                    }
+                  else
+                    {
+                      *arg++ = *input;
+                    }
+                }
+              else if (dquote)
+                {
+                  if (*input == '"')
+                    {
+                      dquote = 0;
+                    }
+                  else
+                    {
+                      *arg++ = *input;
+                    }
+                }
+              else
+                {
+                  if (*input == '\'')
+                    {
+                      squote = 1;
+                    }
+                  else if (*input == '"')
+                    {
+                      dquote = 1;
+                    }
+                  else
+                    {
+                      *arg++ = *input;
+                    }
+                }
+              input++;
+            }
+        }
+      *arg = EOS;
+      argv[argc] = strdup (copybuf);
+      argc++;
+      argv[argc] = NULL;
 
-	  consume_whitespace (&input);
-	}
-      while (*input != EOS);
+      consume_whitespace (&input);
+    }
+  while (*input != EOS);
 
-      free (copybuf);
+  free (copybuf);
   return (argv);
 }
 
