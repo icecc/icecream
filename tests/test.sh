5a4cf1012d18c29c5ffa5019664211aeaba80b29
Luboš Luňák <l.lunak@centrum.cz>
2019-05-31 18:12:25 +0200

    do not require absolute path in debug tests with absolute path
    It may be because of clang's -fdebug-compilation-dir, but gdb here
    now shows the path only as debug/debug2.cpp and not the full path.
    As long as the actual gdb output works (and thus gdb can find the file),
    this doesn't actually matter, so do not require the full path.

--- tests/test.sh
+++ tests/test.sh
@@ -1804,10 +1804,10 @@ fi
 if command -v gdb >/dev/null; then
     if command -v readelf >/dev/null; then
         debug_test "$TESTCXX" "-c -g debug.cpp" "Temporary breakpoint 1, main () at debug.cpp:8"
-        debug_test "$TESTCXX" "-c -g $(pwd)/debug/debug2.cpp" "Temporary breakpoint 1, main () at $(pwd)/debug/debug2.cpp:8"
+        debug_test "$TESTCXX" "-c -g $(pwd)/debug/debug2.cpp" "Temporary breakpoint 1, main () at .*debug/debug2.cpp:8"
         if test -z "$debug_fission_disabled"; then
             debug_test "$TESTCXX" "-c -g debug.cpp -gsplit-dwarf" "Temporary breakpoint 1, main () at debug.cpp:8"
-            debug_test "$TESTCXX" "-c -g $(pwd)/debug/debug2.cpp -gsplit-dwarf" "Temporary breakpoint 1, main () at $(pwd)/debug/debug2.cpp:8"
+            debug_test "$TESTCXX" "-c -g $(pwd)/debug/debug2.cpp -gsplit-dwarf" "Temporary breakpoint 1, main () at .*debug/debug2.cpp:8"
         fi
     fi
 else
