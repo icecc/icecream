b807f1c385e1d5e884297a4021e2f82b4998d394
Luboš Luňák <l.lunak@centrum.cz>
2019-05-31 18:20:21 +0200

    force -ftime-report* and -ftime-trace as local builds
    They are profiling options for the compiler itself.

--- client/arg.cpp
+++ client/arg.cpp
@@ -336,7 +336,9 @@ bool analyse_argv(const char * const *argv, CompileJob &job, bool icerun, list<s
                 log_info() << "preprocessing, building locally" << endl;
             } else if (!strncmp(a, "-fdump", 6)
                        || !strcmp(a, "-combine")
-                       || !strcmp(a, "-fsyntax-only")) {
+                       || !strcmp(a, "-fsyntax-only")
+                       || !strncmp(a, "-ftime-report", strlen("-ftime-report"))
+                       || !strcmp(a, "-ftime-trace")) {
                 always_local = true;
                 args.append(a, Arg_Local);
                 log_info() << "argument " << a << ", building locally" << endl;
