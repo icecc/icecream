icecc/icecream

commit 0c7c455f0454d0ee30f6bd3d7838af93338f951c

Removed dependency on native tar command and used libarchive library (#447)
* Removed dependency on native tar command and used libarchive library instead
* Updated NEWS and README.

daemon/environment.cpp
@@ -38,6 +38,9 @@
 #include "exitcode.h"
 #include "util.h"
 
+#include <archive.h>
+#include <archive_entry.h>
+
 using namespace std;
 
 size_t sumup_dir(const string &dir)
@@ -425,11 +428,37 @@ size_t finish_create_env(int pipe, const string &basedir, string &native_environ
 }
 
 
+
+static int copy_data(struct archive *ar, struct archive *aw)
+{
+    int r;
+    const void *buff;
+    size_t size;
+
+#if ARCHIVE_VERSION_NUMBER >= 3000000
+    int64_t offset;
+#else
+    off_t offset;
+#endif
+    for(;;){
+        r = archive_read_data_block(ar, &buff, &size, &offset);
+        if (r == ARCHIVE_EOF){
+            return (ARCHIVE_OK);
+        }
+        r= archive_write_data_block(aw, buff, size, offset);
+        if(r != ARCHIVE_OK){
+            trace() << "COPY_DATA: Error after write"<< archive_error_string(aw)<<endl;
+            return (r);
+        }
+    }
+}
+
 pid_t start_install_environment(const std::string &basename, const std::string &target,
                                 const std::string &name, MsgChannel *c,
                                 int &pipe_to_stdin, FileChunkMsg *&fmsg,
                                 uid_t user_uid, gid_t user_gid, int extract_priority)
 {
+    log_info() << "start_install_environment: " << basename << " target "<<target << " Name: " << name << endl;
     if (!name.size()) {
         log_error() << "illegal name for environment " << name << endl;
         return 0;
@@ -453,19 +482,6 @@ pid_t start_install_environment(const std::string &basename, const std::string &
     }
 
     fmsg = dynamic_cast<FileChunkMsg*>(msg);
-    const char *decompressor = NULL;
-
-    if (fmsg->len > 2) {
-        if (fmsg->buffer[0] == 037 && fmsg->buffer[1] == 0213) {
-            decompressor = "-z";    // --gzip
-        } else if (fmsg->buffer[0] == 'B' && fmsg->buffer[1] == 'Z') {
-            decompressor = "-j";    // --bzip2
-        } else if (fmsg->buffer[0] == 0xfd && fmsg->buffer[1] == 0x37) {
-            decompressor = "-J";    // --xz
-        } else if (fmsg->buffer[0] == 0x28 && fmsg->buffer[1] == 0xb5) {
-            decompressor = "-Iunzstd";
-        }
-    }
 
     if (mkdir(dirname.c_str(), 0770) && errno != EEXIST) {
         log_perror("mkdir target") << "\t" << dirname << endl;
@@ -489,10 +505,10 @@ pid_t start_install_environment(const std::string &basename, const std::string &
         return 0;
     }
 
-    int fds[2];
+    int fds[2]; //File descriptor for Pipe
 
     if (pipe(fds) == -1) {
-        log_perror("pipe failed");
+        log_perror("start_install_environment: pipe creation failed for recveiving environment");
         return 0;
     }
 
@@ -500,16 +516,17 @@ pid_t start_install_environment(const std::string &basename, const std::string &
     pid_t pid = fork();
 
     if (pid == -1) {
-        log_perror("fork - trying to run tar");
+        log_perror("Failed to fork - trying to run tar");
         return 0;
     }
     if (pid) {
-        trace() << "pid " << pid << endl;
+        //Runs only on parent(PID value is 0 in child and PID id on parent)
+        trace() << "Created fork for recveiving environment on pid " << pid << endl;
 
         if ((-1 == close(fds[0])) && (errno != EBADF)){
-            log_perror("close failed");
+            log_perror("Failed to close read end of pipe");
         }
-        pipe_to_stdin = fds[1];
+        pipe_to_stdin = fds[1]; //Set write end of pipe to pass to parent thread
 
         return pid;
     }
@@ -540,15 +557,16 @@ pid_t start_install_environment(const std::string &basename, const std::string &
     signal(SIGPIPE, SIG_DFL);
 
     if ((-1 == close(0)) && (errno != EBADF)){
-        log_perror("close failed");
+        //https://linux.die.net/man/3/close
+        log_perror("Failed to close standard input");
     }
 
     if ((-1 == close(fds[1])) && (errno != EBADF)){
-        log_perror("close failed");
+        log_perror("Failed to close write end of pipe");
     }
 
     if (-1 == dup2(fds[0], 0)){
-        log_perror("dup2 failed");
+        log_perror("Failed to duplicate file descriptor for read end of pipe with standard input");
     }
 
     int niceval = nice(extract_priority);
@@ -556,16 +574,65 @@ pid_t start_install_environment(const std::string &basename, const std::string &
         log_warning() << "failed to set nice value: " << strerror(errno) << endl;
     }
 
-    char **argv;
-    argv = new char*[5];
-    argv[0] = strdup(TAR);
-    argv[1] = strdup("-xC");
-    argv[2] = strdup(dirname.c_str());
-    argv[3] = decompressor ? strdup(decompressor) : 0;
-    argv[4] = 0;
+    /* libarchive stream reader */
+    struct archive *a;
+    struct archive *ext;
+    struct archive_entry *entry;
+    int flags;
 
-    execv(argv[0], argv);
-    log_perror("execv failed");
+    flags = ARCHIVE_EXTRACT_TIME;
+    flags |= ARCHIVE_EXTRACT_PERM;
+    flags |= ARCHIVE_EXTRACT_ACL;
+    flags |= ARCHIVE_EXTRACT_FFLAGS;
+
+    a=archive_read_new();
+    archive_read_support_format_all(a);
+    archive_read_support_filter_all(a);
+    ext = archive_write_disk_new();
+    archive_write_disk_set_options(ext, flags);
+    archive_write_disk_set_standard_lookup(ext);
+
+    if(archive_read_open_fd(a, 0, fmsg->len)){
+        log_error() << "start_install_environment: "<< "archive_read_open_fd faied"<< endl;
+        _exit(1);
+    }
+
+    for(;;){
+        int r = archive_read_next_header(a, &entry);
+        if (r == ARCHIVE_EOF) {
+            trace() << "start_install_environment: reached end of archive while recveiving environment "<< endl;
+            break;
+        }
+        if (r < ARCHIVE_WARN){
+            log_error() << "start_install_environment: r  < ARCHIVE_WARN " <<archive_error_string(a)<<endl;   
+            _exit(1);
+        }
+
+        /*Extracting archive*/
+        const char* currentFile = archive_entry_pathname(entry);
+        const std::string fullOutputPath = dirname + "/"+currentFile;
+        archive_entry_set_pathname(entry, fullOutputPath.c_str());
+        r = archive_write_header(ext, entry);
+
+        if(archive_entry_size(entry) > 0){
+            r= copy_data(a, ext);
+            if(r < ARCHIVE_WARN){
+                log_error()<< "start_install_environment: " << archive_error_string(ext)<<endl;
+                _exit(1);
+            }
+        }
+        r=archive_write_finish_entry(ext);
+        if(r<ARCHIVE_WARN){
+            log_error() << "start_install_environment: " << archive_error_string(ext)<<endl;
+            _exit(1);
+        }
+    }
+    archive_read_close(a);
+    archive_read_free(a);
+    archive_write_close(ext);
+    archive_write_free(ext);
+    /*libarchive stream reader ends*/
+
     _exit(100);
 }
 
@@ -574,14 +641,12 @@ size_t finalize_install_environment(const std::string &basename, const std::stri
                                     pid_t pid, uid_t user_uid, gid_t user_gid)
 {
     int status = 1;
-
-    while (waitpid(pid, &status, 0) < 0 && errno == EINTR) {}
-
-    if (shell_exit_status(status) != 0) {
-        log_error() << "exit code: " << shell_exit_status(status) << endl;
-        remove_environment(basename, target);
+    if (pid < 1){
+        log_error() << "finalize_install_environment: Invalid pid " << pid << endl;
         return 0;
     }
+    trace() << "finalize_install_environment started for " << basename << " target " << target << " arriving on PID " << pid <<endl;
+    while (waitpid(pid, &status, 0) < 0 && errno == EINTR) {}
 
     string dirname = basename + "/target=" + target;
 
@@ -754,7 +819,7 @@ bool verify_env(MsgChannel *client, const string &basedir, const string &target,
 
         return shell_exit_status(status) == 0;
     } else if (pid < 0) {
-        log_perror("fork failed");
+        log_perror("Failed to fork for verifying environment");
         return false;
     }
 

daemon/main.cpp
@@ -181,7 +181,7 @@ public:
 
         if (pipe_to_child >= 0) {
             if (-1 == close(pipe_to_child) && (errno != EBADF)){
-                log_perror("close failed");
+                log_perror("Failed to close pipe to child process");
             }
         }
 
@@ -201,11 +201,11 @@ public:
 
         switch (status) {
         case LINKJOB:
-            return ret + " CID: " + toString(client_id) + " " + outfile;
+            return ret + " ClientID: " + toString(client_id) + " " + outfile;
         case TOINSTALL:
             return ret + " " + toString(client_id) + " " + outfile;
         case WAITFORCHILD:
-            return ret + " CID: " + toString(client_id) + " PID: " + toString(child_pid) + " PFD: " + toString(pipe_to_child);
+            return ret + " ClientID: " + toString(client_id) + " PID: " + toString(child_pid) + " PFD: " + toString(pipe_to_child);
         case WAITCREATEENV:
             return ret + " " + toString(client_id) + " " + pending_create_env;
         default:
@@ -214,12 +214,12 @@ public:
                 string jobs;
 
                 if (usecsmsg) {
-                    jobs = " CS: " + usecsmsg->hostname;
+                    jobs = " CompileServer: " + usecsmsg->hostname;
                 }
 
-                return ret + " CID: " + toString(client_id) + " ID: " + toString(job_id) + jobs;
+                return ret + " ClientID: " + toString(client_id) + " Job ID: " + toString(job_id) + jobs;
             } else {
-                return ret + " CID: " + toString(client_id);
+                return ret + " ClientID: " + toString(client_id);
             }
         }
 
@@ -327,7 +327,7 @@ static int set_new_pgrp(void)
     int pgrp_id = getpgrp();
 
     if (-1 == pgrp_id){
-        log_perror("getpgrp() failed");
+        log_perror("Failed to get process group ID");
         return EXIT_DISTCC_FAILED;
     }
 
@@ -562,14 +562,14 @@ bool Daemon::setup_listen_fds()
 
     if (!noremote) { // if we only listen to local clients, there is no point in going TCP
         if ((tcp_listen_fd = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
-            log_perror("socket()");
+            log_perror("Failed to create TCP listen socket.");
             return false;
         }
 
         int optval = 1;
 
         if (setsockopt(tcp_listen_fd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval)) < 0) {
-            log_perror("setsockopt()");
+            log_perror("Failed to set 'Reuse Address(SO_REUSEADDR)' option on TCP Listen Socket");
             return false;
         }
 
@@ -583,7 +583,7 @@ bool Daemon::setup_listen_fds()
 
             if (::bind(tcp_listen_fd, (struct sockaddr *)&myaddr,
                      sizeof(myaddr)) < 0) {
-                log_perror("bind()");
+                log_perror("Failed to bind address to TCP listen socket");
                 sleep(2);
 
                 if (!--count) {
@@ -597,7 +597,7 @@ bool Daemon::setup_listen_fds()
         }
 
         if (listen(tcp_listen_fd, 20) < 0) {
-            log_perror("listen()");
+            log_perror("Failed to set TCP socket for listening to incoming connections");
             return false;
         }
 
@@ -605,7 +605,7 @@ bool Daemon::setup_listen_fds()
     }
 
     if ((unix_listen_fd = socket(AF_UNIX, SOCK_STREAM, 0)) < 0) {
-        log_perror("socket()");
+        log_perror("Failed to create a Unix scoket for listening");
         return false;
     }
 
@@ -666,7 +666,7 @@ bool Daemon::setup_listen_fds()
     }
 
     if (::bind(unix_listen_fd, (struct sockaddr*)&myaddr, sizeof(myaddr)) < 0) {
-        log_perror("bind()");
+        log_perror("Failed to bind address to unix listen socket");
 
         if (reset_umask) {
             umask(old_umask);
@@ -680,7 +680,7 @@ bool Daemon::setup_listen_fds()
     }
 
     if (listen(unix_listen_fd, 20) < 0) {
-        log_perror("listen()");
+        log_perror("Failed to set unix socket for listening");
         return false;
     }
 
@@ -694,7 +694,7 @@ void Daemon::determine_system()
     struct utsname uname_buf;
 
     if (uname(&uname_buf)) {
-        log_perror("uname call failed");
+        log_perror("uname call failed. Unable to determine system node name and platform");
         return;
     }
 
@@ -733,7 +733,7 @@ bool Daemon::send_scheduler(const Msg& msg)
     }
 
     if (!scheduler->send_msg(msg)) {
-        log_error() << "sending to scheduler failed.." << endl;
+        log_error() << "sending message to scheduler failed.." << endl;
         close_scheduler();
         return false;
     }
@@ -912,7 +912,7 @@ int Daemon::scheduler_get_internals()
 int Daemon::scheduler_use_cs(UseCSMsg *msg)
 {
     Client *c = clients.find_by_client_id(msg->client_id);
-    trace() << "handle_use_cs " << msg->job_id << " " << msg->client_id
+    trace() << "scheduler_use_cs " << msg->job_id << " " << msg->client_id
             << " " << c << " " << msg->hostname << " " << remote_name <<  endl;
 
     if (!c) {
@@ -947,7 +947,7 @@ int Daemon::scheduler_use_cs(UseCSMsg *msg)
 int Daemon::scheduler_no_cs(NoCSMsg *msg)
 {
     Client *c = clients.find_by_client_id(msg->client_id);
-    trace() << "handle_no_cs " << msg->job_id << " " << msg->client_id
+    trace() << "scheduler_no_cs " << msg->job_id << " " << msg->client_id
             << " " << c << " " <<  endl;
 
     if (!c) {
@@ -969,7 +969,7 @@ int Daemon::scheduler_no_cs(NoCSMsg *msg)
 
 bool Daemon::handle_transfer_env(Client *client, Msg *_msg)
 {
-    log_error() << "handle_transfer_env" << endl;
+    log_error() << "handle_transfer_env while  Client Status" << Client::status_str(client->status) <<  endl;
 
     assert(client->status != Client::TOINSTALL &&
            client->status != Client::TOCOMPILE &&
@@ -994,8 +994,9 @@ bool Daemon::handle_transfer_env(Client *client, Msg *_msg)
     current_kids++;
 
     if (pid > 0) {
-        log_error() << "got pid " << pid << endl;
-        client->pipe_to_child = sock_to_stdin;
+        //in parent thread
+        log_error() << "PID of child thread running untaring environment: " << pid << endl;
+        client->pipe_to_child = sock_to_stdin; //Write end of the pipe obtained from child
         client->child_pid = pid;
 
         if (!handle_file_chunk_env(client, fmsg)) {
@@ -1013,7 +1014,7 @@ bool Daemon::handle_transfer_env(Client *client, Msg *_msg)
 
 bool Daemon::handle_transfer_env_done(Client *client)
 {
-    log_error() << "handle_transfer_env_done" << endl;
+    log_error() << "handle_transfer_env_done PID " << client->child_pid <<" for " << client->outfile << endl;
 
     assert(client->outfile.size());
     assert(client->status == Client::TOINSTALL);
@@ -2308,7 +2309,7 @@ int main(int argc, char **argv)
 
     if (detach)
         if (daemon(0, 0)) {
-            log_perror("daemon()");
+            log_perror("Failed to run as a daemon.");
             exit(EXIT_DISTCC_FAILED);
         }
 

.travis.yml
@@ -41,7 +41,7 @@ matrix:
   - os: osx
     before_install:
       - brew update
-      - brew install lzo docbook2x gdb ccache
+      - brew install lzo docbook2x gdb ccache libarchive
   - os: linux
     sudo: true # for setcap so we can run the tests in chroot.
     compiler: clang
@@ -87,6 +87,12 @@ before_script:
          export UBSAN_OPTIONS=print_stacktrace=1
      fi
   - |
+     if [ "$TRAVIS_OS_NAME" == "osx" ]; then
+         export LDFLAGS="${LDFLAGS} -L/usr/local/opt/libarchive/lib"
+         export CPPFLAGS="${CPPFLAGS} -I/usr/local/opt/libarchive/include"
+         export PKG_CONFIG_PATH="/usr/local/opt/libarchive/lib/pkgconfig"
+     fi
+  - |
       export CFLAGS="${SAN_FLAGS} ${CFLAGS} -Wall -Wextra"
       export CXXFLAGS="${SAN_FLAGS} ${CXXFLAGS} -Wall -Wextra"
       export LDFLAGS="${SAN_FLAGS} ${LDFLAGS}"
@@ -103,3 +109,4 @@ addons:
     - realpath
     - gdb
     - valgrind
+    - libarchive-dev

README
@@ -3,6 +3,12 @@ it depends critically on Linux' /proc/ structures and will fail
 at runtime until somebody figures out how to emulate the "ticks"
 form of process accounting in there.
 
+Required Libraries
+==================
+Note: The package name may vary by distro
+* libcap-ng-devel
+* libarchive-devel
+
 How to install icecream
 =======================
 

configure.ac
@@ -122,6 +122,7 @@ AC_CHECK_HEADERS([netinet/tcp_var.h], [], [],
 ])
 
 AC_CHECK_HEADERS([sys/user.h])
+AC_CHECK_HEADERS([archive.h, archive_entry.h])
 
 ######################################################################
 dnl Checks for types
@@ -190,6 +191,10 @@ AC_SUBST(LZO_LDADD)
 
 AC_CHECK_LIB([dl], [dlsym], [DL_LDADD=-ldl])
 AC_SUBST([DL_LDADD])
+
+AC_CHECK_LIB(archive, archive_read_data_block, ARCHIVE_LDADD=-larchive,
+    AC_MSG_ERROR([Could not find libarchive library - please install libarchive-devel]))
+AC_SUBST(ARCHIVE_LDADD)
 
 # In DragonFlyBSD daemon needs to be linked against libkinfo.
 case $host_os in

daemon/Makefile.am
@@ -12,7 +12,8 @@ iceccd_SOURCES = \
 iceccd_LDADD = \
 	../services/libicecc.la \
 	$(LIB_KINFO) \
-	$(CAPNG_LDADD)
+	$(CAPNG_LDADD) \
+	$(ARCHIVE_LDADD)
 
 AM_CPPFLAGS = \
 	-I$(top_srcdir)/services

commit 7466c80c3a886403004594101e5efaf35e5ed521

fix various typos introduced in 0c7c455f0454

daemon/environment.cpp
@@ -380,7 +380,7 @@ static int copy_data(struct archive *ar, struct archive *aw)
         }
         r= archive_write_data_block(aw, buff, size, offset);
         if(r != ARCHIVE_OK){
-            trace() << "COPY_DATA: Error after write"<< archive_error_string(aw)<<endl;
+            trace() << "copy_data(): Error after write: "<< archive_error_string(aw)<<endl;
             return (r);
         }
     }
@@ -449,7 +449,7 @@ pid_t start_install_environment(const std::string &basename, const std::string &
     pid_t pid = fork();
 
     if (pid == -1) {
-        log_perror("Failed to fork - trying to run tar");
+        log_perror("start_install_environment - fork()");
         return 0;
     }
     if (pid) {
@@ -526,7 +526,7 @@ pid_t start_install_environment(const std::string &basename, const std::string &
     archive_write_disk_set_standard_lookup(ext);
 
     if(archive_read_open_fd(a, 0, fmsg->len)){
-        log_error() << "start_install_environment: "<< "archive_read_open_fd faied"<< endl;
+        log_error() << "start_install_environment: archive_read_open_fd() failed"<< endl;
         _exit(1);
     }
 
@@ -537,7 +537,7 @@ pid_t start_install_environment(const std::string &basename, const std::string &
             break;
         }
         if (r < ARCHIVE_WARN){
-            log_error() << "start_install_environment: r  < ARCHIVE_WARN " <<archive_error_string(a)<<endl;   
+            log_error() << "start_install_environment: r  < ARCHIVE_WARN " <<archive_error_string(a)<<endl;
             _exit(1);
         }
 

daemon/main.cpp
@@ -963,7 +963,7 @@ int Daemon::scheduler_no_cs(NoCSMsg *msg)
 
 bool Daemon::handle_transfer_env(Client *client, Msg *_msg)
 {
-    log_info() << "handle_transfer_env while client status " << Client::status_str(client->status) <<  endl;
+    log_info() << "handle_transfer_env, client status " << Client::status_str(client->status) <<  endl;
 
     assert(client->status != Client::TOINSTALL &&
            client->status != Client::TOCOMPILE &&
@@ -989,7 +989,7 @@ bool Daemon::handle_transfer_env(Client *client, Msg *_msg)
 
     if (pid > 0) {
         //in parent thread
-        trace() << "PID of child thread running untaring environment: " << pid << endl;
+        trace() << "PID of child thread unpacking environment: " << pid << endl;
         client->pipe_to_child = sock_to_stdin; //Write end of the pipe obtained from child
         client->child_pid = pid;
 