4b824aab604d0c438f79ed6f9a4e4cf6c826543e
Luboš Luňák <l.lunak@centrum.cz>
2020-07-04 17:59:41 +0200

    build locally also with -save-temps=<argument>

--- client/arg.cpp
+++ client/arg.cpp
@@ -440,8 +440,10 @@ bool analyse_argv(const char * const *argv, CompileJob &job, bool icerun, list<s
                        || !strcmp(a, "-fprofile-use")
                        || !strcmp(a, "-save-temps")
                        || !strcmp(a, "--save-temps")
+                       || str_startswith(a, "-save-temps=")
+                       || str_startswith(a, "--save-temps=")
                        || !strcmp(a, "-fbranch-probabilities")) {
-                log_info() << "compiler will emit profile info (argument " << a << "); building locally" << endl;
+                log_info() << "compiler will emit additional local files (argument " << a << "); building locally" << endl;
                 always_local = true;
                 args.append(a, Arg_Local);
             } else if (!strcmp(a, "-gsplit-dwarf")) {
